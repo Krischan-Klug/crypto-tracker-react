87e2a45ca8a567ed92762277c6c9c3bb6cf3eb06
Krischan-Klug <krischanklug@icloud.com>
2024-02-29 16:09:25 +0100

    added dynamic coin update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "https://api4.binance.com/api/v3/ticker/24hr"
+      );
+      const data = await response.json();
+
+      return data;
+    }
+
+    async function dataTest() {
+      const allCoins = await getData();
+      setCoins(allCoins);
+    }
+
+    const interval1 = setInterval(() => {
+      dataTest();
+      console.log(coins);
+      console.log("interval start");
+    }, 5000);
+
+    return () => {
+      console.log("cleanup");
+      clearInterval(interval1);
+    };
+  }, []);
+
   return (
     <>
       <Header />
-      <Main />
+      <Main coins={coins} />
       <Footer />
     </>
   );
